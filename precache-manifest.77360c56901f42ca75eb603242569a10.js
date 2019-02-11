e53f4aceb16112536784fa5399d830f8e2886d59
unknown <547557356@qq.com>
2019-02-12 11:20:10 +1300

    Updates

---
+++ precache-manifest.77360c56901f42ca75eb603242569a10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a80337c646dd63d589f0",
+    "url": "/reactdemo-cra/static/js/runtime~main.a80337c6.js"
+  },
+  {
+    "revision": "f068b80e8ae7a3926c44",
+    "url": "/reactdemo-cra/static/js/main.f068b80e.chunk.js"
+  },
+  {
+    "revision": "b521472ebcc0f0e98ced",
+    "url": "/reactdemo-cra/static/js/2.b521472e.chunk.js"
+  },
+  {
+    "revision": "f068b80e8ae7a3926c44",
+    "url": "/reactdemo-cra/static/css/main.15cca6c0.chunk.css"
+  },
+  {
+    "revision": "73a96aa6d284e81c45107f13025a4eea",
+    "url": "/reactdemo-cra/index.html"
+  }
+];
